e3807e5acee888fb88d35db8b051aa0af85b3df2
iltramont <124902413+iltramont@users.noreply.github.com>
2024-07-07 21:16:42 +0200

    updates

--- utils.py
+++ utils.py
@@ -19,3 +19,15 @@ def compute_min_variance_weights(covariance_matrix: pd.DataFrame) -> pd.Series:
 
 def compute_returns(df: pd.DataFrame | pd.Series) -> pd.DataFrame | pd.Series:
     return (df - df.shift(1)) / df.shift(1)
+
+
+def compute_single_period_returns(period_stocks_returns: pd.DataFrame, weights: pd.Series) -> pd.DataFrame:
+    index = period_stocks_returns.index
+    columns = period_stocks_returns.columns
+    matrix = np.empty(period_stocks_returns.shape)
+    result = pd.DataFrame(matrix, index=index, columns=columns)
+    # result.iloc[0, :] = weights * period_stocks_returns.iloc[0, :]
+    result.iloc[0, :] = weights
+    for i in range(1, len(index)):
+        result.iloc[i, :] = result.iloc[i - 1, :] * (1 + period_stocks_returns.iloc[i, :])
+    return result
